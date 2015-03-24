a81db950b297dc6c3e8c99026917035fdf6154d4
Poorva Rane <poorvarane99@gmail.com>
2015-03-24 19:22:04 +0530

    Update Lexer.java

--- src/rubyinterpreter/Lexer.java
+++ src/rubyinterpreter/Lexer.java
@@ -1,23 +1,36 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package rubyinterpreter;
-import java.util.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.io.*;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
 /**
- *
- * @author nidhi
- */
-public class Lexer {
+*
+* @author nidhi
+*/
+public class Lexer extends JFrame {
     Set setKeywords, setOperators, setComparators, setLogical;
-    
+    String lexicalInformation="";
+    JTextPane textPane;
     Lexer()
-    {
+    {   
+        textPane = new JTextPane();
+        setSize(1375,725);
+        setTitle("RUBY INTERPRETER");
+        setBackground(Color.RED);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        create();
         try {
             setKeywords = new HashSet();
             setOperators = new HashSet();
@@ -27,62 +40,57 @@ public class Lexer {
             Scanner operatorFile = new Scanner(new File("arithmeticoperators.txt"));
             Scanner comparatorFile = new Scanner(new File("comparisonoperators.txt"));
             Scanner logicalFile = new Scanner(new File("logicaloperators.txt"));
-
+            
             while(keyFile.hasNext())
             {
                 String word = keyFile.next();
                 setKeywords.add(word);
-                
             }
-             while(operatorFile.hasNext())
+            while(operatorFile.hasNext())
             {
                 String word = operatorFile.next();
                 setOperators.add(word);
-                
             }
-             while(comparatorFile.hasNext())
+            while(comparatorFile.hasNext())
             {
                 String word = comparatorFile.next();
                 setComparators.add(word);
-                
             }
             while(logicalFile.hasNext())
             {
                 String word = logicalFile.next();
                 setLogical.add(word);
-                
             }
             Scanner file = new Scanner(new File("ruby.txt"));
-            
             while(file.hasNext())
             {
                 String token = file.next();
                 token = token.toLowerCase();
                 if(setKeywords.contains(token))
-                System.out.println(token + " is a keyword \n");
+                    lexicalInformation+=token + " is a keyword \n\n";
                 else if(setOperators.contains(token))
-                    System.out.println(token + " is an arithmetic operator \n");
+                    lexicalInformation+=token + " is an arithmetic operator \n\n";
                 else if(token.equals("="))
-                    System.out.println(token + " is an assignment operator \n");
+                    lexicalInformation+=token + " is an assignment operator \n\n";
                 else if(setComparators.contains(token))
-                    System.out.println(token + " is a comparison operator \n");
+                    lexicalInformation+=token + " is a comparison operator \n\n";
                 else if(setLogical.contains(token))
-                    System.out.println(token + " is a logical operator \n");
+                    lexicalInformation+=token + " is a logical operator \n\n";
                 else if(token.matches("[0-9*]"))
-                    System.out.println(token + " is a constant \n");
+                    lexicalInformation+=token + " is a constant \n\n";
                 else if(token.matches("^[a-zA-Z_@$][a-zA-Z0-9_]*$"))
-                    System.out.println(token + " is a variable \n");
-                else                    
-                    System.out.println(token + " \n");
- 
-             
-
+                    lexicalInformation+=token + " is a variable \n\n";
+                else
+                    lexicalInformation+=token + " \n\n";
             }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Lexer.class.getName()).log(Level.SEVERE, null, ex);
+            textPane.setText(lexicalInformation);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Lexer.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
     }
-    
-    
+        private void create() {
+        JPanel mainPanel = (JPanel) getContentPane();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(textPane);
+    }
 }
